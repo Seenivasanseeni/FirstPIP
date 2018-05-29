50633d47260f8fdd6e6cea368217efb13274d506
SeenivasanSeeni <seeni0424@gmail.com>
2018-05-29 10:48:22 +0530

    Initial Version

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+with open("README.md","r") as fh:
+    long_description=fh.read()
+
+setuptools.setup(
+    name="FirstPIP",
+    version="0.0.1",
+    author="Seeni",
+    author_email="seeni0424@gmail.com"
+)
